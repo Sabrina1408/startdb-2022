1f1ffe8c2005a9713bf3137836f9d029eb3dd86d
Sabrina Alves <sabrina.a.brito@gmail.com>
2022-07-21 22:08:08 -0300

    desafio feito

--- desafio/forca.js
+++ desafio/forca.js
@@ -1,16 +1,54 @@
 class Forca {
+  palavraSecreta
+  letrasChutadas = []
+  palavra = []
+  vidas = 6
 
-  chutar(letra) { }
+  constructor(pSecreta) { /* Quando o new no forca executar vai jogar o valor no parametro*/
+    this.palavraSecreta = pSecreta
+    for (let i in this.palavraSecreta) {
+      this.palavra.push('_');
+    }
+  }
 
-  buscarEstado() { return ""; } // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
+  chutar(letra) {
+    if (letra.length != 1) {
+      console.log('Chute apenas uma letra')
+      console.log(`Letra chutada: ${this.letrasChutadas}`)
+      return
+    }
+    if (this.letrasChutadas.includes(letra) == true) {
+      console.log('Essa letra já foi utilizada');
+      return
+    }
+    for (let i in this.palavraSecreta) {
+      if(this.palavraSecreta[i]==letra){
+        this.palavra[i] = letra;
+      }
+    }
+    this.letrasChutadas.push(letra)
+    if (this.palavra.includes(letra) == false) {
+      this.vidas--;
+    }
+    //console.log(`Letra chutada: ${this.letrasChutadas}`)
+  }
+
+  buscarEstado() {
+    
+    if (this.palavra.indexOf('_') == -1) {
+      return 'ganhou';
+    } else if (this.vidas == 0) {
+      return 'perdeu';
+    }
+  } // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
 
   buscarDadosDoJogo() {
-      return {
-          letrasChutadas: [], // Deve conter todas as letras chutadas
-          vidas: 6, // Quantidade de vidas restantes
-          palavra: [] // Deve ser um array com as letras que já foram acertadas ou o valor "_" para as letras não identificadas
-      }
+    return {
+      letrasChutadas: this.letrasChutadas, // Deve conter todas as letras chutadas
+      vidas: this.vidas, // Quantidade de vidas restantes
+      palavra: this.palavra // Deve ser um array com as letras que já foram acertadas ou o valor "_" para as letras não identificadas
+    }
   }
 }
 
-module.exports = Forca;
+module.exports = Forca
